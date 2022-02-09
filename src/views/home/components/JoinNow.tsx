f03b06ce1ab39c562d8cf5dcbf4647af238cf797
WINDOWS-88VV8LL\user <tellerg@crocusoft.com>
2022-02-10 00:12:21 +0400

    Add JoinNow component

---
+++ src/views/home/components/JoinNow.tsx
@@ -0,0 +1,7 @@
+import { ButtonType, PrimaryButton } from '../../../components/PrimaryButton';
+
+export const JoinNow: React.FC = () => (
+  <>
+    <PrimaryButton title="Join now" type={ButtonType.GRADIENT} />
+  </>
+);
