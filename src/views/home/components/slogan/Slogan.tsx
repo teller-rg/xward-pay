0542144c4214026a7930e29434d74bccaeb61bf0
WINDOWS-88VV8LL\user <tellerg@crocusoft.com>
2022-02-11 05:08:26 +0400

    Add content to Slogan component

---
+++ src/views/home/components/slogan/Slogan.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  ButtonType,
+  PrimaryButton,
+} from '../../../../components/primaryButton/PrimaryButton';
+
+import JoinNowImage from '../../../../assets/images/slogan.svg';
+import './Slogan.css';
+
+export const Slogan: React.FC = () => (
+  <>
+    <div className="container text-center slogan">
+      <div className="slogan-content">
+        <h3 className="title-primary">
+          A dimension where everything is possible
+        </h3>
+        <p className="text-primary">
+          Xward allows seamless and compliant integration in multiple Virtual
+          and Augment environments by embedding our financial connections right
+          to the core of digital world.
+        </p>
+        <PrimaryButton title="Learn More" type={ButtonType.PRIMARY} />
+      </div>
+      <div className="slogan-background">
+        <img src={JoinNowImage} alt="" />
+      </div>
+    </div>
+  </>
+);
