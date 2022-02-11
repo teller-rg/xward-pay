4dfda73c576fa64a87c8b67caafc020525803687
Tellər R. Quluzadə <teller.quluzade@cybernet.az>
2022-02-11 11:29:05 +0400

    Rename state variables

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,15 +5,15 @@ import HeaderLogo from '../../assets/images/header_logo.svg';
 import './Header.css';
 
 export const Header: React.FC = () => {
-  const [open, setOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <div className="header container">
       <Link to="/">
         <img src={HeaderLogo} alt="" />
       </Link>
       <div
-        className={`menu-icon ${open ? 'menu-icon-close' : ''}`}
-        onClick={() => setOpen(open ? false : true)}
+        className={`menu-icon ${isMenuOpen ? 'menu-icon-close' : ''}`}
+        onClick={() => setMenuOpen(!isMenuOpen)}
       >
         <div className="bar"></div>
         <div className="bar"></div>
