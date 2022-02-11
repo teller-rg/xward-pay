be368d0577043025e4bf726afd4511b8bb89f665
WINDOWS-88VV8LL\user <tellerg@crocusoft.com>
2022-02-11 05:17:15 +0400

    Move Header and Footer into BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Header } from './components/Header';
+import React from 'react';
+import { Header } from './components/header/Header';
 import { HomeView } from './views/home/HomeView';
-import { Footer } from './components/Footer';
+import { Footer } from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
@@ -8,13 +9,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomeView />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
