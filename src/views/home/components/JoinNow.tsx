1b784f13fa54414daf5c2dcd486c4f8e604a8fd8
WINDOWS-88VV8LL\user <tellerg@crocusoft.com>
2022-02-11 05:06:19 +0400

    Add content to JoinNow component

--- src/views/home/components/JoinNow.tsx
+++
@@ -1,7 +0,0 @@
-import { ButtonType, PrimaryButton } from '../../../components/PrimaryButton';
-
-export const JoinNow: React.FC = () => (
-  <>
-    <PrimaryButton title="Join now" type={ButtonType.GRADIENT} />
-  </>
-);
---
+++ src/views/home/components/joinnow/JoinNow.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  ButtonType,
+  PrimaryButton,
+} from '../../../../components/primaryButton/PrimaryButton';
+import { TextField } from '../../../../components/textField/TextField';
+
+import JoinNowImage from '../../../../assets/images/joinNow.svg';
+import './JoinNow.css';
+
+export const JoinNow: React.FC = () => (
+  <>
+    <div className="container text-center joinNow">
+      <h1 className="title-primary">
+        Future of the <strong>Metaverse</strong> Banking
+      </h1>
+      <p className="text-primary">
+        Xward offers most complete solutions for embedding banking features in
+        your Metaverse ecosystem
+      </p>
+      <div className="form-group">
+        <TextField placeholder="Enter your email" />
+        <PrimaryButton title="Join now" type={ButtonType.GRADIENT} />
+      </div>
+      <div className="joinNow-background">
+        <img src={JoinNowImage} alt="" />
+      </div>
+    </div>
+  </>
+);
